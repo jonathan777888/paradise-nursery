5df0706be00e323463ae0757155c8e2b58f4116a
jonathan777888 <kpakpojonathan37@gmail.com>
2026-06-14 13:41:08 -0400

    Add AboutUs component for company information

---
+++ src/src/AboutUs.jsx
@@ -0,0 +1,25 @@
+function AboutUs() {
+  return (
+    <div className="about-page">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery is an online plant store dedicated to helping people
+        bring nature into their homes and workspaces.
+      </p>
+
+      <p>
+        We offer a wide selection of indoor plants, including air-purifying
+        plants, succulents, and flowering plants. Our mission is to provide
+        beautiful, healthy, and affordable plants for every customer.
+      </p>
+
+      <p>
+        At Paradise Nursery, we believe plants can improve mood, purify the air,
+        and make every space feel more peaceful and welcoming.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
